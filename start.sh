63f5e8d3ac8583e9a063d964f1be3b561e061ff2
wpfuse <113634078+wp-fuse@users.noreply.github.com>
2026-03-05 18:23:25 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,35 +7,38 @@ if [ ! -d /data/wp-content ]; then
     cp -a /app/public/wp-content /data/
 fi
 
-# Copia o php.ini customizado para o disco se ele não existir
+# Copia o php.ini customizado para o disco
 if [ ! -f /data/server/php.ini ]; then
-    echo 'Criando php.ini editavel no disco...'
+    echo 'Criando php.ini no disco...'
     cp /usr/local/etc/php/conf.d/php.ini.base /data/server/php.ini
 fi
 
-# 2) Move o wp-config.php para o disco persistente
-if [ ! -e /data/server/wp-config.php ]; then
-    echo 'Movendo wp-config.php para a pasta de configuracoes...'
+# 2) Constrou o wp-config.php
+if [ ! -f /data/server/wp-config.php ]; then
+    echo 'Construindo o wp-config.php persistente...'
     cp /app/public/wp-config.php /data/server/wp-config.php
+    SALTS=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
+    awk -v salts="$SALTS" '{gsub(/REPLACE_WP_SALTS_HERE/, salts)}1' /data/server/wp-config.php > /tmp/wp.tmp
+    mv /tmp/wp.tmp /data/server/wp-config.php
 fi
 
-# 3) Garante que o arquivo público seja SEMPRE um symlink apontando para o disco
-# Remove o que quer que esteja lá (seja arquivo original ou link quebrado) e recria o link
+# Cria o Symlink
+rm -f /app/public/wp-config.base.php
 rm -f /app/public/wp-config.php
 ln -sf /data/server/wp-config.php /app/public/wp-config.php
 
-# 4) Processa a infraestrutura do Caddy
+# 3) Processa a infraestrutura do Caddy
 ARQUIVO_CADDY=${CADDYFILE_NAME:-Caddyfile}
 CADDY_DEST="/data/server/$ARQUIVO_CADDY"
 
 cat /etc/caddy/Caddyfile > "$CADDY_DEST"
 sed -i -e '/import \/etc\/caddy\/snippets\/webdav.caddy/r /etc/caddy/snippets/webdav.caddy' -e '/import \/etc\/caddy\/snippets\/webdav.caddy/d' "$CADDY_DEST"
 
-# 5) Processa a Autenticacao
+# 4) Processa a Autenticacao
 if [ -n "$WEBDAV_USER" ] && [ -n "$WEBDAV_HASH" ]; then
     echo "Injetando credenciais do WebDAV a partir das variáveis de ambiente"
     sed -i "s|placeholder_user|$WEBDAV_USER|g" "$CADDY_DEST"
-    sed -i "s|pL4CeH0Ld3r-Ha5h-So-NaO-Usar|$WEBDAV_HASH|g" "$CADDY_DEST"
+    sed -i "s|placeholder_hash|$WEBDAV_HASH|g" "$CADDY_DEST"
 else
     echo "Aviso: Senhas ausentes. Trancando WebDAV com chaves aleatórias únicas..."
 
@@ -46,8 +49,8 @@ else
 
     # Injeta os valores gerados dinamicamente no arquivo
     sed -i "s|placeholder_user|$RANDOM_USER|g" "$CADDY_DEST"
-    sed -i "s|pL4CeH0Ld3r-Ha5h-So-NaO-Usar|$RANDOM_HASH|g" "$CADDY_DEST"
+    sed -i "s|placeholder_hash|$RANDOM_HASH|g" "$CADDY_DEST"
 fi
 
-# 6) Executa o servidor
+# 5) Executa o servidor
 exec frankenphp run --config "$CADDY_DEST"
