89b2bf526f10ed39ec6c7de525ac11488b9be969
wpfuse <113634078+wp-fuse@users.noreply.github.com>
2026-03-05 15:05:13 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
 
-# 1) Prepara /data na primeira execução
+# 1) Prepara a pasta /data na primeira execução
 if [ ! -d /data/wp-content ]; then
-    echo 'Preparando disco /data...'
+    echo 'Preparando disco persistente /data...'
     mkdir -p /data/server
     cp -a /app/public/wp-content /data/
 fi
 
+# Define qual nome o Caddy vai usar para buscar as configurações
 ARQUIVO_CADDY=${CADDYFILE_NAME:-Caddyfile}
 CADDY_DEST="/data/server/$ARQUIVO_CADDY"
 
-# 2) Sempre constrói um Caddyfile fresco unindo o base e os snippets
-cat /etc/caddy/Caddyfile.base > "$CADDY_DEST"
-# Resolve o "import" do Caddy concatenando o snippet de WebDAV diretamente no arquivo
+# 2) Sempre constrói um Caddyfile fresco a partir da imagem para garantir atualizações
+cat /etc/caddy/Caddyfile > "$CADDY_DEST"
+# Injeta as regras do WebDAV fisicamente dentro do arquivo final 
 sed -i -e '/import \/etc\/caddy\/snippets\/webdav.caddy/r /etc/caddy/snippets/webdav.caddy' -e '/import \/etc\/caddy\/snippets\/webdav.caddy/d' "$CADDY_DEST"
 
-# 3) Se tiver env vars, sobrescreve o placeholder no arquivo final
+# 3) Processa a Autenticação
 if [ -n "$WEBDAV_USER" ] && [ -n "$WEBDAV_HASH" ]; then
-    echo "Injetando credenciais do WebDAV a partir das variáveis de ambiente..."
+    echo "Injetando credenciais do WebDAV a partir do Magic Containers..."
     sed -i "s|placeholder_user|$WEBDAV_USER|g" "$CADDY_DEST"
     sed -i "s|pL4CeH0Ld3r-Ha5h-So-NaO-Usar|$WEBDAV_HASH|g" "$CADDY_DEST"
 else
-    echo "Aviso: WEBDAV_USER/WEBDAV_HASH ausentes. WebDAV bloqueado com credenciais de fabrica."
+    echo "Aviso: Senhas ausentes. WebDAV permanecerá selado com credenciais de fábrica."
 fi
 
+# 4) Executa o servidor
 exec frankenphp run --config "$CADDY_DEST"
